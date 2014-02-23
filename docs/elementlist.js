2dc0d74b1507856032aa56ebbcd23eb85aad5ed5
Bobby Allen <ballen@bobbyallen.me>
2014-02-23 13:06:33 +0000

    Updated library API documentation with updates as per latest code updates.

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Ballen\\Gravel\\Gravatar"]];
+ApiGen.elements = [["c","Ballen\\Gravel\\Facades\\Gravatar"],["c","Ballen\\Gravel\\Facades\\GravatarMapper"],["c","Ballen\\Gravel\\Gravatar"],["c","Ballen\\Gravel\\GravelServiceProvider"]];
